47e3f0dbf1674bb36e097911ec799960b2261c67
Jonny <jonny@garrill.com>
2026-07-20 17:12:29 +0100

    Initial Commit

---
+++ Current/CurrentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrentApp.swift
+//  Current
+//
+//  Created by Jonny Garrill on 20/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct CurrentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CurrentTests/CurrentTests.swift
@@ -0,0 +1,19 @@
+//
+//  CurrentTests.swift
+//  CurrentTests
+//
+//  Created by Jonny Garrill on 20/07/2026.
+//
+
+import Testing
+@testable import Current
+
+struct CurrentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
